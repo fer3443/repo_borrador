66eb10dd546afb0ed068468091795b6314f90d31
Fernando <fer3443@gmail.com>
2023-08-02 22:37:01 -0300

    xx

---
+++ repo_borrador/src/components/login/Login.jsx
@@ -0,0 +1,100 @@
+import { useState, useContext } from 'react';
+
+import  { Login } from '../../services/user_service';
+
+import'../login/ModalLogin.css';
+import { DataProvider } from '../../context/DataContext';
+
+export default function ModalLogin({setLogin}) {
+  const { data: {setUserData}, sesion :{isLogged, setIsLogged} } = useContext(DataProvider);
+  
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    allowsLocaStorage: false,
+  });
+  //useState para boton user
+  const [loginButton, setLoginButton] = useState(false);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    Login({
+      email: user.email,
+      password: user.password,
+    })
+      .then((res) => {
+        console.log(res)
+        setUserData(res)
+        setIsLogged(true)
+      })
+      .catch((error) => console.log(error))
+      .finally(setShowModal(false))
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <div className='containerLoginGral'>
+      <button className='btn-login' onClick={toggleModal}>Login</button>
+      <div className={ showModal? 'modalContainer active': 'modalContainer'}>
+        <div className='modalContent'>
+          <button className='btnCerrar' onClick={() => {
+            setLogin(false)
+            setShowModal(false)
+            setLoginButton(false)
+            }}>cerrar</button>
+          <h3>Inicio de Sesión</h3>
+          <form>
+            <label>
+              Ingresa tu mail:
+              <input
+                type='email'
+                name='email'
+                onChange={(e) => setUser({
+                  ...user,
+                  email: e.target.value,
+                  })
+                }
+              />
+            </label>
+            <label>
+              Contraseña:
+              <input
+                type='password'
+                name='password'
+                onChange={(e) => setUser({
+                  ...user,
+                  password: e.target.value,
+                  })
+                }
+              />
+            </label>
+            <label>
+              Mantenerme conectado
+              <input 
+                type='checkbox'
+                name='checkbox'
+                onClick={(e) => setUser({
+                  ...user,
+                  allowsLocaStorage: e.target.checked,
+                })}
+              />
+            </label>
+            <button className='btnSesion' type='button' onClick={() => {
+              handleLogin
+              setLogin(false)
+              } }>
+              Iniciar Sesión
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ repo_borrador/src/components/navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import {useState} from 'react'
+
+import { Link, NavLink } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCartShopping, faCircleQuestion, faHeart, faBars } from '@fortawesome/free-solid-svg-icons';
+
+import '../navegacion/ObsidianNavbar.css'
+import FilterComponent from '../filtroNavegacion/FilterComponent';
+import Favoritos from '../favoritos/Favoritos';
+import ModalLogin from '../login/ModalLogin';
+
+export const ObsidianNavbar = () => {
+
+//useState para menu burger
+  const [clicked, setClicked] = useState(false)
+
+  function clickear(){
+    setClicked(!clicked)
+  }
+//useState para boton user
+  const [login, setLogin] = useState(false)
+//para cambiar de color el navbar
+  const [scrolling, setSCrolling] = useState(false)
+
+  function changeBackG() {
+    if(window.scrollY >= 10){
+      setSCrolling(true);
+    }else{
+      setSCrolling(false);
+    }
+  }
+  function openedUser(){
+    setLogin(!login)
+  }
+
+  window.addEventListener("scroll",changeBackG);
+
+  return (
+    <>
+    <div className='navBarBox'>
+      <div className={ scrolling ? 'navBarContainer scroll' : 'navBarContainer'}>
+        <h2>Obsidian<span>-</span>Tech</h2>
+        <div className={ clicked ? 'linksContainer active': 'linksContainer'}>
+          <NavLink className='linkStyle' to={'/'}>Home</NavLink>
+          <NavLink className='linkStyle' to={'/administracion'}>Administracion</NavLink>
+          <NavLink className='linkStyle' to={'/nosotros'}>Nosotros</NavLink>
+          <NavLink className='linkStyle' to={'/recContraseña'}>Recuperar Contraseña</NavLink>
+        </div>
+        <div className='halfContainer'>
+          <div className='iconContainer'>
+            <div className='userContainer'>
+              <button onClick={openedUser}><FontAwesomeIcon icon={faUser} /></button>
+              <div className={ login ? 'loginContainer active' : 'loginContainer'}>
+                <ModalLogin setLogin={setLogin}/>
+                <Link to={"/registro"} className='linkStyle' onClick={openedUser}>Registro </Link>
+              </div>
+            </div>
+            <button><FontAwesomeIcon icon={faCartShopping} /></button>
+            <Link to="/favoritos" className='fav'><FontAwesomeIcon icon={faHeart} /></Link>
+            <button><FontAwesomeIcon icon={faCircleQuestion} /></button>
+          </div>
+          <div className='burger'>
+            <button onClick={clickear}><FontAwesomeIcon icon={faBars} /></button>
+          </div>
+        </div>
+        <div className={clicked ? 'inputContainer active':'inputContainer'}>
+          <FilterComponent/>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
